2de45155644043f1e95ba10b3ff75acaa69a6447
Kyle Cogan <kyle.cogan1993@gmail.com>
2024-02-21 11:47:30 -0500

    wrote slice feature

---
+++ src/features/sortSlice.js
@@ -0,0 +1,37 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState = {
+    bank: [],
+    odds: [],
+    evens: []
+};
+
+const sortSlice = createSlice({
+    name: slice,
+    initialState,
+    reducers : {
+        addNumber: (state, {nums}) => {
+            nums.forEach(num => {
+                state.bank.push(num)
+            });
+        },
+        sort1: (state, {nums}) => {
+            const num = nums.pop();
+            num % 2 === 0 ? state.evens.push(num) : state.odds.push(num);
+        },
+        sortAll: (state, {nums}) => {
+            while(nums.length != 0) {
+            const num = nums.pop();
+            num % 2 === 0 ? state.evens.push(num) : state.odds.push(num);
+            }
+        }
+    }
+});
+
+export const { addNumber, sort1, sortAll } = sortSlice.actions;
+
+export const selectBank = (state) => state.slice.bank;
+export const selectOdds = (state) => state.slice.odds;
+export const selectEvens = (state) => state.slice.evens;
+
+export default sortSlice.reducer;
